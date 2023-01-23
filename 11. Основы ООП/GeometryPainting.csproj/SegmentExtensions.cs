25f2681d01dc63fe16d42cb3f4bfaa5949aa9401
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-01-23 19:19:46 +0500

    Основы ООП

---
+++ 11. Основы ООП/GeometryPainting.csproj/SegmentExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+using System.Drawing;
+
+namespace GeometryPainting
+{
+    public static class SegmentExtensions
+    {
+        public static Dictionary<Segment, Color> Colors = new Dictionary<Segment, Color>();
+
+        public static Color GetColor(this Segment segment)
+        {
+            if (Colors.ContainsKey(segment))
+            {
+                return Colors[segment];
+            }
+            else
+            {
+                return Color.Black;
+            }
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            if (Colors.ContainsKey(segment))
+            {
+                Colors[segment] = color;
+            }
+            else
+            {
+                Colors[segment] = new Color();
+                Colors[segment] = color;
+            }
+        }
+    }
+}
