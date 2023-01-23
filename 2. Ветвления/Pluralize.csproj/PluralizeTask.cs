fa140a1fd0b58b2ba63bf6def67e05e2937237e6
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-01-23 18:17:18 +0500

    Ветвления

---
+++ 2. Ветвления/Pluralize.csproj/PluralizeTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			switch (count / 10 % 10 == 1 ? 0 : count % 10)
+			{
+				case 1: return "рубль"; break;
+
+				case 2: return "рубля"; break;
+
+				case 3: return "рубля"; break;
+
+				case 4: return "рубля"; break;
+
+				default: return "рублей"; break;
+			}
+		}
+	}
+}
---
+++ 2. Ветвления/Rectangles.csproj/RectanglesTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Rectangles
+{
+    public static class RectanglesTask
+    {
+        // Пересечение + границы
+        public static bool AreIntersected(Rectangle r1, Rectangle r2)
+        {
+            return Math.Min(r1.Bottom, r2.Bottom) >= Math.Max(r1.Top, r2.Top) &&
+                    Math.Min(r1.Right, r2.Right) >= Math.Max(r1.Left, r2.Left);
+        }
+
+        // Площадь пересечения
+        public static int IntersectionSquare(Rectangle r1, Rectangle r2)
+        {
+            if (AreIntersected(r1, r2))
+            {
+                return (Math.Min(r1.Bottom, r2.Bottom) - Math.Max(r1.Top, r2.Top)) * (Math.Min(r1.Right, r2.Right) - Math.Max(r1.Left, r2.Left));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
+        {
+            if ((r1.Top >= r2.Top) && (r1.Bottom <= r2.Bottom) && (r1.Left >= r2.Left) && (r1.Right <= r2.Right))
+            {
+                return 0;
+            }
+            else if ((r1.Top <= r2.Top) && (r1.Bottom >= r2.Bottom) && (r1.Left <= r2.Left) && (r1.Right >= r2.Right))
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+}
